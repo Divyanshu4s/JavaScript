08a33a29d65cb5d85b8f6c1b6a84cb3c5a500357
Divyanshu Kumar <om.dhn00@gmail.com>
2026-04-06 06:16:37 +0000

    Datatype created

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Divyanshu")
+
+
+let name = "Divyanshu"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value 
+//eg-> when we use temp it auto assign to 0, but that too is a temp
+
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
